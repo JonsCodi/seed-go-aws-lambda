24cd26bad521fec8dc5f227c8fb4f06210364faf
Jonas Gomes <jonasgoms@hotmail.com>
2022-02-11 16:51:48 -0300

    feat: Using it;

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"github.com/aws/aws-lambda-go/events"
+	"seed-aws-lambda/handler"
+)
+
 func main() {
-	println("Hellow...")
+	handler.NewHandler(events.DynamoDBEvent{})
+	handler.NewHandler(events.APIGatewayProxyRequest{})
+	handler.NewHandler(events.ALBTargetGroupRequest{})
+	handler.NewHandler(events.S3Event{})
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module seed-aws-lambda
 
 go 1.17
+
+require github.com/aws/aws-lambda-go v1.28.0
