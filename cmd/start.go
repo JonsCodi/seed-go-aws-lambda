60994dee6d2e5e7d25d20a5d8aff4aee9e0ce44c
Jonas Gomes <jonasgoms@hotmail.com>
2022-03-08 09:32:02 -0300

    feat: New flags for start command;

--- cmd/start.go
+++ cmd/start.go
@@ -2,14 +2,28 @@ package cmd
 
 import (
 	"github.com/spf13/cobra"
+	"os"
 )
 
 var startCmd = &cobra.Command{
 	Use: "start",
+	Run: func(cmd *cobra.Command, args []string) {
+	},
 }
 
 func init() {
 	rootCmd.AddCommand(startCmd)
 
-	startCmd.PersistentFlags().StringP("env-file", "e", "", "config file")
+	startCmd.PersistentFlags().StringP("env-file", "e", "", "Config environment file")
+	startCmd.PersistentFlags().StringP("name", "n", "", "Name of the project, example (gitlab.com/lambda_project")
+	startCmd.PersistentFlags().StringP("product", "p", "", "Product name that project will be represented")
+	startCmd.PersistentFlags().StringP("description", "d", "", "Any description about it")
+}
+
+func validateFlag(value, message string) {
+	if value == "" {
+		println(message)
+
+		os.Exit(1)
+	}
 }
